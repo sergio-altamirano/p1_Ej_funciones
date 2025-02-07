ba28c5bb276bab7db4a3fe1e546cac8ddb1112e9
sergio-altamirano <sergiodamianaltamirano@gmail.com>
2025-02-07 16:47:53 +0000

    mi primera vez

---
+++ funcion1.dart
@@ -0,0 +1,19 @@
+//funcion que no devuelve valor
+//funcion saludar
+void saludar(){
+  print("hola mundo");
+}
+
+void escribeNombre(){
+  print("sergio altamirano");
+}
+
+void main(){
+  //sergio altamirano 22308051281041
+  //llamada a la funcion saludar
+  print("sergio altamirano 22308051281041");
+  print("llamada a la funcion saludar");
+  saludar();
+  escribeNombre();
+  print("llamando a la funcion escribe nombre");
+}
