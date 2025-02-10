df9cb738014fd147166d78a70c2426fca323ef7a
sergio-altamirano <sergiodamianaltamirano@gmail.com>
2025-02-10 18:33:43 +0000

    publique funcion flecha

---
+++ funcioflecha.dart
@@ -0,0 +1,23 @@
+int multiplicacion (int a, int b){
+  return a * b;
+}
+
+int flechamultiplicacion(int a , int b) => a*b;
+double divicion (int a, int b){
+  return a/b;
+}
+
+double flechadivicion(int a , int b) => a/b;
+//returnType functionName(parameters...)=>expression;
+//funcion main
+void main(){
+    print('SERGIO DAMIAN ALTAMIRANO MAT:22308051281041');
+    print('llamando a la funcion multiplicacion');
+    print(multiplicacion(10, 5));
+    print("llamando a la funcion flechamultiplicacion");
+    print(divicion(10, 5));
+    print("llamando a la funcion divicion");
+    print(flechadivicion(3, 5));
+    print("llamando a la funcion flechadivicion");
+    print(divicion(10, 5));
+}
--- recursos/funcionsuma.md
+++ recursos/funcionsuma.md
@@ -1,2 +1,5 @@
 ejemplo de salida
 ![alt text](image.png)
+ejemplo salida funcion
+normal+ funcion flecha
+![alt text](image-1.png)
