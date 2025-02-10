b0a076b19a6d2da09636434c560594bebeb0641c
sergio-altamirano <sergiodamianaltamirano@gmail.com>
2025-02-10 18:54:04 +0000

    funcion partametros

--- funcionparam.dart
+++ funcionparam.dart
@@ -8,7 +8,7 @@ String concatenar(a, b) {
   return a + b;
 }
 void main() {
-  int suma = sumar(5, 3);
+  int suma = sumar(100, 200);
   print('La suma es: $suma');
 
   String textoConcatenado = concatenar('sergio', 'altamirano');
--- recursos/funcionsuma.md
+++ recursos/funcionsuma.md
@@ -3,3 +3,5 @@ ejemplo de salida
 ejemplo salida funcion
 normal+ funcion flecha
 ![alt text](image-1.png)
+funcion con parametro
+![alt text](image-2.png)
