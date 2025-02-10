fa83d97421353d2d221312d15444b399f3506745
sergio-altamirano <sergiodamianaltamirano@gmail.com>
2025-02-10 18:48:58 +0000

    funcion parametros

---
+++ funcionparam.dart
@@ -0,0 +1,16 @@
+// Función 1
+int sumar(int a, int b) {
+  return a + b;
+}
+
+//funcion 2
+String concatenar(a, b) {
+  return a + b;
+}
+void main() {
+  int suma = sumar(5, 3);
+  print('La suma es: $suma');
+
+  String textoConcatenado = concatenar('sergio', 'altamirano');
+  print('Texto concatenado: $textoConcatenado'); 
+}
